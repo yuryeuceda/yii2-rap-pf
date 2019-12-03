82ce5b798989fcdda0cf758db5e3e8813e293466
lobardales <40684068+lobardales@users.noreply.github.com>
2019-12-03 13:38:10 -0600

    Update pantallas.php

--- views/site/pantallas.php
+++ views/site/pantallas.php
@@ -38,30 +38,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="panel panel-primary">
 
-        <?php
-        NavBar::begin([
-            'brandLabel' => 'My Company',
-
-            'options' => [
-                'class' => 'navbar-inverse navbar-fixed-top',
-            ],
-        ]);
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'items' => [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-                Yii::$app->user->isGuest ?
-                    ['label' => 'Login', 'url' => ['/site/login']] : [
-                        'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                        'url' => ['/site/logout'],
-                        'linkOptions' => ['data-method' => 'post']
-                    ],
-            ],
-        ]);
-        NavBar::end();
-        ?>
         <div class="panel-heading">
             <h3 class="panel-title">Ficha de solicitud de Creditos</h3>
         </div>
